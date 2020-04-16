c3cf26067253c382d22beabd6c876fd700c58c3b
Takkasila Saichol <takkasila@outlook.com>
2020-04-16 19:05:37 +0700

    [EP/18] Constant Buffer

--- hw3d/Graphics.cpp
+++ hw3d/Graphics.cpp
@@ -2,6 +2,7 @@
 #include "dxerr.h"
 #include <sstream>
 #include <d3dcompiler.h>
+#include <cmath>
 
 namespace wrl = Microsoft::WRL;
 
@@ -98,7 +99,7 @@ void Graphics::ClearBuffer( float red, float green, float blue ) noexcept
 	pContext->ClearRenderTargetView( pTarget.Get(), color );
 }
 
-void Graphics::DrawTestTriangle()
+void Graphics::DrawTestTriangle( float angle )
 {
 	HRESULT hr;
 
@@ -126,7 +127,7 @@ void Graphics::DrawTestTriangle()
 		{ -0.5f,-0.5f,0,0,255,0 },
 		{ -0.3f,0.3f,0,255,0,0 },
 		{ 0.3f,0.3f,0,0,255,0 },
-		{ 0.0f,-0.8f,255,0,0,0 },
+		{ 0.0f,-1.0f,255,0,0,0 },
 	};
 	vertices[0].color.g = 255;
 	//
@@ -180,6 +181,40 @@ void Graphics::DrawTestTriangle()
 	//	bind index buffer
 	pContext->IASetIndexBuffer( pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u );
 
+
+	//	create constant buffer for matrix transformation
+	struct ConstantBuffer
+	{
+		struct 
+		{
+			float element[4][4];
+		} transformation;
+	};
+
+	const ConstantBuffer cb =
+	{
+		{
+			( 3.0f / 4.0f ) * std::cos( angle ),	std::sin( angle ),	0,		0,
+			( 3.0f / 4.0f ) * -std::sin( angle ),	std::cos( angle ),	0,		0,
+			0,					0,					1,		0,
+			0,					0,					0,		1,
+		}
+	};
+
+	wrl::ComPtr<ID3D11Buffer> pConstantBuffer;
+	D3D11_BUFFER_DESC cbd;
+	cbd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cbd.Usage = D3D11_USAGE_DYNAMIC;
+	cbd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	cbd.MiscFlags = 0u;
+	cbd.ByteWidth = sizeof( cb );
+	cbd.StructureByteStride = 0u;
+	D3D11_SUBRESOURCE_DATA csd = {};
+	csd.pSysMem = &cb;
+	GFX_THROW_INFO( pDevice->CreateBuffer( &cbd, &csd, &pConstantBuffer ) );
+
+	//	bind constant buffer to vertex shader
+	pContext->VSSetConstantBuffers( 0, 1, pConstantBuffer.GetAddressOf() );
 
 	//
 	//	SHADER PROGRAM
