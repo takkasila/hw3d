bf4e35c2603b4a757b8de6a591b67d1350dcf394
Takkasila Saichol <takkasila@outlook.com>
2020-04-28 21:49:06 +0700

    [EP/24_2] Add Cylinder

--- hw3d/Box.cpp
+++ hw3d/Box.cpp
@@ -11,17 +11,7 @@ Box::Box( Graphics& gfx,
 	std::uniform_real_distribution<float>& rdist,
 	std::uniform_real_distribution<float>& bdist,
 	DirectX::XMFLOAT3 material )
-	:
-	r( rdist( rng ) ),
-	droll( ddist( rng ) ),
-	dpitch( ddist( rng ) ),
-	dyaw( ddist( rng ) ),
-	dphi( odist( rng ) ),
-	dtheta( odist( rng ) ),
-	dchi( odist( rng ) ),
-	chi( adist( rng ) ),
-	theta( adist( rng ) ),
-	phi( adist( rng ) )
+	:	TestObject( gfx, rng, adist, ddist, odist, rdist )
 {
 	namespace dx = DirectX;
 
@@ -64,10 +54,10 @@ Box::Box( Graphics& gfx,
 
 	struct PSMaterialConstant
 	{
-		alignas(16) dx::XMFLOAT3 color;
+		dx::XMFLOAT3 color;
 		float specularIntensity = 0.6f;
 		float specularPower = 30.0f;
-		float padding[2];
+		float padding[3];
 	} colorConst;
 	colorConst.color = material;
 	AddBind(std::make_unique<PixelConstantBuffer<PSMaterialConstant>>(gfx, colorConst, 1u));
@@ -78,21 +68,8 @@ Box::Box( Graphics& gfx,
 	);
 }
 
-void Box::Update( float dt ) noexcept
-{
-	roll += droll * dt;
-	pitch += dpitch * dt;
-	yaw += dyaw * dt;
-	theta += dtheta * dt;
-	phi += dphi * dt;
-	chi += dchi * dt;
-}
-
 DirectX::XMMATRIX Box::GetTransformXM() const noexcept
 {
 	namespace dx = DirectX;
-	return dx::XMLoadFloat3x3( &mt ) *
-		dx::XMMatrixRotationRollPitchYaw( pitch, yaw, roll ) *
-		dx::XMMatrixTranslation( r, 0.0f, 0.0f ) *
-		dx::XMMatrixRotationRollPitchYaw( theta, phi, chi );
+	return dx::XMLoadFloat3x3(&mt) * TestObject::GetTransformXM();
 }
