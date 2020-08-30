8cf391d681ec4df4b41d3fcc53b3a3d7ce07bacb
takkasila <takkasila@outlook.com>
2020-08-30 09:46:24 +0700

    [EP/38] Anisotropic filtering

--- hw3d/Texture.cpp
+++ hw3d/Texture.cpp
@@ -22,31 +22,44 @@ namespace Bind
 		D3D11_TEXTURE2D_DESC textureDesc = {};
 		textureDesc.Width = s.GetWidth();
 		textureDesc.Height = s.GetHeight();
-		textureDesc.MipLevels = 1;
+		textureDesc.MipLevels = 0;
 		textureDesc.ArraySize = 1;
 		textureDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
 		textureDesc.SampleDesc.Count = 1;
 		textureDesc.SampleDesc.Quality = 0;
 		textureDesc.Usage = D3D11_USAGE_DEFAULT;
-		textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+		textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
 		textureDesc.CPUAccessFlags = 0;
-		textureDesc.MiscFlags = 0;
+		textureDesc.MiscFlags = D3D11_RESOURCE_MISC_GENERATE_MIPS;
 
-		D3D11_SUBRESOURCE_DATA sd = {};
-		sd.pSysMem = s.GetBufferPtr();
-		sd.SysMemPitch = s.GetWidth() * sizeof( Surface::Color );
+		//D3D11_SUBRESOURCE_DATA sd = {};
+		//sd.pSysMem = s.GetBufferPtr();
+		//sd.SysMemPitch = s.GetWidth() * sizeof( Surface::Color );
 
 		wrl::ComPtr<ID3D11Texture2D> pTexture;
-		GFX_THROW_INFO( GetDevice( gfx )->CreateTexture2D( &textureDesc, &sd, &pTexture ) );
+		GFX_THROW_INFO( GetDevice( gfx )->CreateTexture2D( &textureDesc, nullptr, &pTexture ) );
+
+		//	write image data into top mip level
+		GetContext( gfx )->UpdateSubresource(
+			pTexture.Get(),
+			0u,
+			nullptr,
+			s.GetBufferPtrConst(),
+			s.GetWidth() * sizeof( Surface::Color ),
+			0u
+		);
 
 		//	creat the resource view on the texture 
 		D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
 		srvDesc.Format = textureDesc.Format;
 		srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
 		srvDesc.Texture2D.MostDetailedMip = 0;
-		srvDesc.Texture2D.MipLevels = 1;
+		srvDesc.Texture2D.MipLevels = -1;
 
 		GFX_THROW_INFO( GetDevice( gfx )->CreateShaderResourceView( pTexture.Get(), &srvDesc, &pTextureView ) );
+
+		// generate the mipmap chain using the gpu rendering pipeline
+		GetContext( gfx )->GenerateMips( pTextureView.Get() );
 	}
 
 	void Texture::Bind( Graphics& gfx ) noexcept
@@ -69,5 +82,12 @@ namespace Bind
 	bool Texture::HasAlpha() const noexcept
 	{
 		return hasAlpha;
+	}
+
+	UINT CalculateNumberOfMipLevels( UINT width, UINT height ) noexcept
+	{
+		const float xSteps = std::ceil( log2( (float)width ) );
+		const float ySteps = std::ceil( log2( (float)height ) );
+		return (UINT)std::max( xSteps, ySteps );
 	}
 }
